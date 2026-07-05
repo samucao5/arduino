767eb818c440fec0bbde0e99c21db81cb1cb55ae
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-05 09:52:44 -0300

    Add files via upload

---
+++ comandos/variaveis_praticas.ino
@@ -0,0 +1,21 @@
+int numero_int = 5;
+float numero_float = -5.23;
+double numero_double = 32;
+char caractere = 's';
+String conjunto_de_caracteres = "samuel";
+
+unsigned int numero_unsigned_int = 5;
+unsigned long numero_unsigned_long = 4;
+
+byte meuByte = 200;
+word myWord = 65000;
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
